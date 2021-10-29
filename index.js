e08abf21c483bae72c02faa0a11211b68b265e06
sabbir0029 <mdsabbirhossain0029@gmail.com>
2021-10-29 22:38:59 +0600

    new add

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json())
 
 // mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dywnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
@@ -21,25 +20,32 @@ async function run(){
     await client.connect();
     const database = client.db("SA-Travel");
     const dataCollection = database.collection("data");
+    const dataHome = database.collection('HomeServices');
 
     // get Api
     app.get('/services',async(req,res)=>{
       const cursol = dataCollection.find({});
       const page = req.query.page;
       const size = parseInt(req.query.size);
-      let product
+      let services
       const count = await cursol.count();
       if(page){
-        product = await cursol.skip(page*size).limit(size).toArray();
+        services = await cursol.skip(page*size).limit(size).toArray();
       }
       else{
-         product = await cursol.toArray();
+        services = await cursol.toArray();
       }
       
       res.send({
         count,
-        product});
+        services});
     });
+
+    app.get('/Homeservices', async (req, res)=>{
+      const data = dataHome.find({})
+      const services = await data.limit(6).toArray()
+      res.send(services)
+    })
 
         // POST API
         // app.post('/services',async(req,res)=>{
